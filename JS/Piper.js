e9260b32018ae0a0ee749e13e933eda849f9e32c
emanueg <87157798+emanueg@users.noreply.github.com>
2023-02-28 13:00:24 -0300

    Add files via upload

---
+++ JS/Piper.js
@@ -0,0 +1,47 @@
+
+document.addEventListener('scroll', function() {
+    var image = document.getElementById("fotito");
+  
+    image.style.width = '50px';
+    image.style.height = 'auto';
+  });
+
+  
+  document.getElementById("fotito").onclick= function() {
+
+    var image = document.getElementById("fotito");
+  
+    image.style.width = '155px';
+    image.style.height = 'auto';
+  };
+ 
+
+  document.getElementById("boton1").onclick= function() {
+
+    var image = document.getElementById("fotito");
+  
+    image.style.width = '155px';
+    image.style.height = 'auto';
+  };
+  document.getElementById("boton2").onclick= function() {
+
+    var image = document.getElementById("fotito");
+  
+    image.style.width = '155px';
+    image.style.height = 'auto';
+  };
+  document.getElementById("boton3").onclick= function() {
+
+    var image = document.getElementById("fotito");
+  
+    image.style.width = '155px';
+    image.style.height = 'auto';
+  };
+  document.getElementById("boton4").onclick= function() {
+
+    var image = document.getElementById("fotito");
+  
+    image.style.width = '155px';
+    image.style.height = 'auto';
+  };
+
